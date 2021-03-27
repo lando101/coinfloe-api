c4256016b9369c71876b8bc8ef140aefe8d35140
Messmer-Landon <messmer_landon@bah.com>
2021-03-27 16:22:32 -0400

    updates

--- coinfloe-api/src/crypto/routes.js
+++ coinfloe-api/src/crypto/routes.js
@@ -1,9 +1,80 @@
 const express = require('express');
 const fetch = require('node-fetch');
+var ccxt = require('ccxt');
+const ccxws = require("ccxws");
+const kraken = new ccxws.kraken();
+const publicKey = '870e32b5723764a8f9c470f7a2c27ce118fde8d43477cdc618d67d803707fa84';   // e.g. 12326758a39a720e15d064cab3c1f0a9332d107de453bd41926bb3acd565059e
+const privateKey = 'e4d2bd8098e82609e84e5b8d44572dee8768a4aad1001ea5ac2d5062687578a1133b4fd92f64cf110e3a1975d442f55f8f93fd8d03594ff1e32ba8d07f05bb95'; // e.g. 6991cf4c9b518293429db0df6085d1731074bed8abccd7f0279a52fac5b0c1a8a2f6d28e11a50fbb1c6575d1407e637f9ad7c73fbddfa87c5d418fd58971f829
+const Shrimpy = require('shrimpy-node');
+const privateClient = new Shrimpy.ShrimpyApiClient(publicKey, privateKey);
+
+let apiClient = null;
+let wsClient = null;
+let token = null;
+
+console.log(ccxt.exchanges);
+
 
 const routes = express.Router({
     mergeParams: true
 });
+
+// privateClient
+//     .getTicker('kucoin')
+//     .then(data => {
+//         // do something with the data
+//         console.log(data);
+//     })
+//     .catch(error => {
+//          // handle the error
+//     });
+// function handler(msg){
+//     console.log(msg);
+// };
+
+// function subscribeWhenConnected(oData){
+
+//     if (wsClient.getReadyState() === 1) {
+//         console.log("Subcribing to the order book for ETH-BTC");
+//         wsClient.subscribe(oData, handler);
+//     } else {
+//         console.log("waiting for ws connection...");
+//         setTimeout(subscribeWhenConnected.bind(null, oData), 1000);
+//     }
+
+// };
+
+// function unsubscribe(oData){
+//     console.log("Unsubcribing now");
+//     wsClient.unsubscribe(oData);
+//     console.log("Stopping the application");
+//     process.exit(1);
+// };
+
+// (async () => {
+
+//     apiClient = new Shrimpy.ShrimpyApiClient(publicKey, privateKey);
+//     token = await apiClient.getToken();
+//     wsClient = new Shrimpy.ShrimpyWsClient(function (error) {
+//         console.error(error);
+//     }, token);
+
+//     wsClient.connect();
+//     subscribeWhenConnected({
+//         "type": "subscribe",
+//         "pair": "ETH-BTC",
+//         "exchange": "coinbasepro",
+//         "channel": "orderbook"
+//     });
+
+//     setTimeout(unsubscribe.bind(null, {
+//         "type": "unsubscribe",
+//         "pair": "ETH-BTC",
+//         "exchange": "coinbasepro",
+//         "channel": "orderbook"
+//     }), 10000);
+
+// })();
 
 const crypto_api_key = "641209cc5125f295360f388673546b58ea5e5a6d26846d4b05bd03d61ef8e4f2";
 
